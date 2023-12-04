24e86bdc51f2ec11bb3829efdb4ae33064bbc2a1
jasongriller <ja194255@ucf.edu>
2023-12-03 19:40:05 -0500

    Get interactive discs

--- ConnectFour/app/src/main/java/com/example/connectfour/BoardFragment.java
+++ ConnectFour/app/src/main/java/com/example/connectfour/BoardFragment.java
@@ -1,23 +1,44 @@
 package com.example.connectfour;
 
 import androidx.annotation.NonNull;
+import androidx.core.content.ContextCompat;  // Add this import
 import androidx.fragment.app.Fragment;
+import androidx.core.graphics.drawable.DrawableCompat;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.GridLayout;
+import android.widget.Toast;
 
 public class BoardFragment extends Fragment {
     private final String GAME_STATE = "gameState";
     private ConnectFourGame mGame;
     private GridLayout mGrid;
 
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View parentView = inflater.inflate(R.layout.fragment_board, container, false);
+
+        // Instantiate the member variable of class ConnectFourGame
+        mGame = new ConnectFourGame();
+
+        if (savedInstanceState == null) {
+            // Call method startGame
+            mGame.startGame();
+        } else {
+            // Declare local variable, data type String, to store the game state (i.e., gameState)
+            String gameState = savedInstanceState.getString(GAME_STATE);
+
+            // Call method setState in class ConnectFourGame, pass local variable gameState as an argument
+            mGame.setState(gameState);
+
+            // Call method setDisc
+            // Assuming you have a method setDisc in ConnectFourGame class, replace it with the actual method
+            mGame.setDisc(); // replace with actual method call
+        }
 
         // Add the same click handler to all grid buttons
         mGrid = parentView.findViewById(R.id.board_grid);
@@ -35,6 +56,79 @@ public class BoardFragment extends Fragment {
         int buttonIndex = mGrid.indexOfChild(view);
         int row = buttonIndex / ConnectFourGame.ROW;
         int col = buttonIndex % ConnectFourGame.COL;
+
+        // Call method selectDisc in class ConnectFourGame
+        mGame.selectDisc(row, col);
+
+        // Call method setDisc
+        mGame.setDisc();
+
+        // If method isGameOver in class ConnectFourGame returns true
+        if (mGame.isGameOver()) {
+            // Instantiate a Toast to congratulate player if game is won
+            congratsToast();
+
+            // Call method newGame in class ConnectFourGame
+            mGame.newGame();
+
+            // Call method setDisc
+            mGame.setDisc();
+        }
+    }
+
+    private void congratsToast() {
+        Toast.makeText(getContext(), "Congrats! You Won!", Toast.LENGTH_SHORT).show();
+    }
+
+    private void startGame() {
+        // Call method newGame in class ConnectFourGame
+        mGame.newGame();
+
+        // Call method setDisc
+        mGame.setDisc();
+    }
+    private void setDisc() {
+        // Iterate through the collection mGrid.getChildCount()
+        for (int buttonIndex = 0; buttonIndex < mGrid.getChildCount(); buttonIndex++) {
+            Button gridButton = (Button) mGrid.getChildAt(buttonIndex);
+
+            // Find the button's row and column
+            int row = buttonIndex / ConnectFourGame.COL;
+            int col = buttonIndex % ConnectFourGame.COL;
+
+            // Instantiate an instance of class Drawable for the three drawable discs in res/drawable
+            int drawableResourceId;
+            switch (mGame.getDisc(row, col)) {
+                case ConnectFourGame.BLUE:
+                    drawableResourceId = R.drawable.circle_blue;
+                    break;
+                case ConnectFourGame.RED:
+                    drawableResourceId = R.drawable.circle_red;
+                    break;
+                case ConnectFourGame.EMPTY:
+                default:
+                    drawableResourceId = R.drawable.circle_white;
+                    break;
+            }
+
+            // Instantiate an instance of class Drawable using class.method ContextCompat.getDrawable
+            Drawable discDrawable = ContextCompat.getDrawable(requireActivity(), drawableResourceId);
+
+            // Set each Drawable object equal to DrawableCompat.wrap
+            discDrawable = DrawableCompat.wrap(discDrawable);
+
+            // Get the value of the element stored in the current row and column
+            int discValue = mGame.getDisc(row, col);
+
+            // Based on the value of the element (i.e., BLUE, RED, EMPTY),
+            switch (discValue) {
+                case ConnectFourGame.BLUE:
+                case ConnectFourGame.RED:
+                case ConnectFourGame.EMPTY:
+                    gridButton.setBackgroundResource(drawableResourceId);
+                    break;
+            }
+        }
     }
 
     @Override
--- ConnectFour/app/src/main/java/com/example/connectfour/ConnectFourGame.java
+++ ConnectFour/app/src/main/java/com/example/connectfour/ConnectFourGame.java
@@ -6,12 +6,16 @@ public class ConnectFourGame {
     public static final int ROW = 7;
     public static final int COL = 6;
     public static final int EMPTY = 0;
-    public static final int YELLOW = 1;
+    public static final int BLUE = 1;
     public static final int RED = 2;
+    public static final int DISCS = 42;
+
     private int[][] boardGrid;
+    private int player;
 
     public ConnectFourGame() {
         boardGrid = new int[ROW][COL];
+        player = BLUE;
     }
 
     public void newGame() {
@@ -20,6 +24,7 @@ public class ConnectFourGame {
                 boardGrid[row][col] = EMPTY;
             }
         }
+        player = BLUE;
     }
     public String getState() {
         StringBuilder boardString = new StringBuilder();
@@ -31,4 +36,124 @@ public class ConnectFourGame {
 
         return boardString.toString();
     }
+
+    // Added method getDisc
+    public int getDisc(int row, int col) {
+        return boardGrid[row][col];
+    }
+
+    // Add method isGameOver
+    public boolean isGameOver() {
+        return isBoardFull() || isWin();
+    }
+
+    // Add method isWin
+    public boolean isWin() {
+        return checkHorizontal() || checkVertical() || checkDiagonal();
+    }
+
+    // Helper method to check if the board is full
+    private boolean isBoardFull() {
+        for (int col = 0; col < COL; col++) {
+            if (boardGrid[0][col] == EMPTY) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    // Helper method for checking win conditions
+    private boolean checkHorizontal() {
+        for (int row = 0; row < ROW; row++) {
+            for (int col = 0; col < COL - 3; col++) {
+                int disc = boardGrid[row][col];
+                if (disc != EMPTY &&
+                        disc == boardGrid[row][col + 1] &&
+                        disc == boardGrid[row][col + 2] &&
+                        disc == boardGrid[row][col + 3]) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    // Helper method for checking vertical win conditions
+    private boolean checkVertical() {
+        for (int row = 0; row < ROW - 3; row++) {
+            for (int col = 0; col < COL; col++) {
+                int disc = boardGrid[row][col];
+                if (disc != EMPTY &&
+                        disc == boardGrid[row + 1][col] &&
+                        disc == boardGrid[row + 2][col] &&
+                        disc == boardGrid[row + 3][col]) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    // Helper method for checking diagonal win conditions
+    private boolean checkDiagonal() {
+        return checkDiagonalTopLeftToBottomRight() || checkDiagonalTopRightToBottomLeft();
+    }
+
+    // Helper method for checking diagonal from top-left to bottom-right
+    private boolean checkDiagonalTopLeftToBottomRight() {
+        for (int row = 0; row < ROW - 3; row++) {
+            for (int col = 0; col < COL - 3; col++) {
+                int disc = boardGrid[row][col];
+                if (disc != EMPTY &&
+                        disc == boardGrid[row + 1][col + 1] &&
+                        disc == boardGrid[row + 2][col + 2] &&
+                        disc == boardGrid[row + 3][col + 3]) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    // Helper method for checking diagonal from top-right to bottom-left
+    private boolean checkDiagonalTopRightToBottomLeft() {
+        for (int row = 0; row < ROW - 3; row++) {
+            for (int col = 3; col < COL; col++) {
+                int disc = boardGrid[row][col];
+                if (disc != EMPTY &&
+                        disc == boardGrid[row + 1][col - 1] &&
+                        disc == boardGrid[row + 2][col - 2] &&
+                        disc == boardGrid[row + 3][col - 3]) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public void setState(String gameState) {
+        int index = 0;
+        for (int row = 0; row < ROW; row++) {
+            for (int col = 0; col < COL; col++) {
+                boardGrid[row][col] = Character.getNumericValue(gameState.charAt(index));
+                index++;
+            }
+        }
+    }
+
+    public void selectDisc(int row, int col) {
+        for (int i = ROW - 1; i >= 0; i--) {
+            if (boardGrid[i][col] == EMPTY) {
+                boardGrid[i][col] = player;
+                switchPlayer();
+                break;
+            }
+        }
+    }
+
+    private void switchPlayer() {
+        player = (player == BLUE) ? RED : BLUE;
+    }
+
+
 }
